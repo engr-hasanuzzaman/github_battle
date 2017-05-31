f7782c75064280c7c46a5852230e96342430ec12
engr.hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2017-05-31 14:55:16 +0600

    add routing

--- app/components/App.js
+++ app/components/App.js
@@ -3,15 +3,33 @@
  */
 var React = require('react');
 var Popular = require('./Popular');
+var ReactRouter = require('react-router-dom');
+var Router = ReactRouter.BrowserRouter;
+var Route = ReactRouter.Route;
+var Path = ReactRouter.Path;
+var Nav = require('./Nav');
+var Home = require('./Home');
+var Battle = require('./Battle');
+var Switch = ReactRouter.Switch;
 
 class App extends React.Component{
   render(){
     return(
-      <div className='container'>
-      <ul>
-        <Popular />
-       </ul>
-    </div>
+      <Router>
+        <div className='container'>
+            <Nav />
+            <Switch>
+              <Route exact path='/' component={Home} />
+              <Route exact path='/popular' component={Popular} />
+              <Route path='/battle' component={Battle} />
+              <Route render={function() {
+                return <p> Your page not found! </p>
+              }} />
+            </Switch>
+
+         </div>
+      </Router>
+
   )
   }
 }
---
+++ app/components/Battle.js
@@ -0,0 +1,13 @@
+var React = require('react');
+
+class Battle extends React.Component{
+  render(){
+    return(
+      <div>
+        this is battle
+      </div>
+    );
+  }
+}
+
+module.exports = Battle;
