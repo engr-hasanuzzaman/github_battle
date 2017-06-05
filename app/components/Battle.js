9a18cd6de5be4e223cbf15bb060023bfb2f1b6cc
engr-hasanuzzaman <hasanuzzaman.sumon@gmail.com>
2017-06-05 18:10:02 +0600

    create player input functionality

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,10 +1,84 @@
 var React = require('react');
+var PropTypes = require('prop-types');
+
+class PlayerInput extends React.Component{
+  constructor(props){
+    super(props)
+
+    this.state = {
+      username: '',
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleSubmit(event){
+    event.preventDefault();
+    console.log('handl submint' + this.state.username);
+    this.props.onSubmit(this.props.id, this.state.username);
+  }
+
+  handleChange(event){
+    var value = event.target.value;
+
+    this.setState(function() {
+      return { username: value };
+    });
+  }
+
+    render(){
+        return(
+          <form className='column' onSubmit={this.handleSubmit}>
+            <label> {this.props.label}</label>
+            <input
+              value={this.state.username}
+              onChange={this.handleChange}
+              placeholder='input github username'
+              type='text'
+            />
+            <button onSubmit={this.handleSubmit}>
+              Submit
+            </button>
+          </form>
+        );
+    }
+}
+
+PlayerInput.PropTypes = {
+  id: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  onSubmit: PropTypes.func.isRequired,
+}
 
 class Battle extends React.Component{
+  constructor(props){
+    super(props);
+
+    this.state = {
+      first_name: '',
+      first_pic: '',
+      second_name: '',
+      second_pic: ''
+    }
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(id, name){
+    this.setState(function() {
+      var newState = {};
+      newState[id + '_name'] = name;
+      newState[id + '_pic'] = 'https://github.com/' + name + '.png?size=200'
+      return newState;
+    });
+  }
+
   render(){
     return(
       <div>
-        this is battle
+        {!this.state.first_name && <PlayerInput label='Input first username' id='first' onSubmit={this.handleSubmit}/>}
+        {!this.state.second_name && <PlayerInput label='Input second username' id='second' onSubmit={this.handleSubmit}/> }
       </div>
     );
   }
